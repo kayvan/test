51efaa0f2cdb603ae0bcb1d27bf7591f581f5f06
kayvan <k.ebadzadeh@gmail.com>
2026-01-03 12:31:18 +0100

    remove vscode items

--- 01-var-loop-func.cjs
+++ 01-var-loop-func.cjs
@@ -1,14 +1,14 @@
-const { groupEnd } = require("console")
+import { groupEnd } from "console";
 
 console.log("hellllooo!!!")
 // Variables
 const name = "Akbar"
 age = 33.5
-let isMale= true
+let isMale = true
 age = 13
 // Fellow controllers + loop
 console.log("Name: " + name, "Age: " + age)
-if (age > 18 ) {
+if (age > 18) {
     console.log("you can proceed.")
 } else {
     console.log("boro khoone bache!!!")
@@ -21,7 +21,7 @@ if (isMale) {
 }
 
 // functions
-function getName (firstname, lastname) {
+function getName(firstname, lastname) {
     return firstname + " " + lastname
 }
 const greeting = (fname, lname) => { //anonymous func
@@ -29,14 +29,14 @@ const greeting = (fname, lname) => { //anonymous func
 }
 console.log(greeting("Mamad", "Jafari"))
 // Euler1: sum of every number below 1000 that multiples 3 or 5
-function euler1 (num){
-    let sum = 0 
-    for (var i = 0 ; i < num ; i++){
-        if (i % 3 == 0 || i % 5 == 0){
+function euler1(num) {
+    let sum = 0
+    for (var i = 0; i < num; i++) {
+        if (i % 3 == 0 || i % 5 == 0) {
             sum += i
             //console.log("Sum: " + sum)
         }
     }
     return sum
-}  
+}
 console.log("Sum is " + euler1(10000))
